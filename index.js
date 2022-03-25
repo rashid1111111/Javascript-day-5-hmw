10f46f2953df77405e6585abbbdd3e0ad9b71818
rashid1111111 <rashidbek0309@gmail.com>
2022-03-26 01:08:15 +0500

    Add search and filter section

--- index.js
+++ index.js
@@ -3,58 +3,89 @@ const firstName = document.querySelector('.first-name')
 const lastName = document.querySelector('.last-name')
 const age = document.querySelector('.age')
 const select = document.querySelector('.form-select')
-const list = document.querySelector('.list')
-
+const list = document.querySelector('.thbody')
+const search = document.querySelector('.search')
+const filter = document.querySelector('.filter')
+const buttonFilter = document.querySelector('.button-filter')
 let information = [
-    {
-    id: 1,
-    name: 'Ahror',
-    lastName: 'Rashidov',
-    study: 'android',
-    age: 20
-    },
-    {
-    id: 2,
-    name: 'Sobir',
-    lastName: 'Hamdamov',
-    study: 'design',
-    age: 22
-    }
+    {id: 1, name: 'Rashidbek', lastName: 'Xoldarov', age: 22, study: 'frontEnd'}
 ]
 
-createEl()
+createEl(information, list)
 
-function createEl() {
-    information.map((item) => {
-        let newLi = document.createElement('li')
-        let nameSpan = document.createElement('span')
-        let lastNameSpan = document.createElement('span')
-        let ageSpan = document.createElement('span')
-        let studySpan = document.createElement('span')
+function createEl(arr, list) {
+    list.textContent = null
 
-        nameSpan.textContent = item.name
-        lastNameSpan.textContent = item.lastName
-        ageSpan.textContent = item.age
-        studySpan.textContent = item.study
-        newLi.appendChild(nameSpan)
-        newLi.appendChild(lastNameSpan)
-        newLi.appendChild(ageSpan)
-        newLi.appendChild(studySpan)
-        list.appendChild(newLi)
+    arr.map((item) => {
+        let newTr = document.createElement('tr')
+        let nameTh = document.createElement('th')
+        let lastNameTh = document.createElement('th')
+        let idTh = document.createElement('th')
+        let ageTh = document.createElement('th')
+        let studyTh = document.createElement('th')
+
+        nameTh.textContent = item.name
+        lastNameTh.textContent = item.lastName
+        idTh.textContent = item.id
+        ageTh.textContent = item.age
+        studyTh.textContent = item.study
+        newTr.appendChild(idTh)
+        newTr.appendChild(nameTh)
+        newTr.appendChild(lastNameTh)
+        newTr.appendChild(ageTh)
+        newTr.appendChild(studyTh)
+        list.appendChild(newTr)
+
     })
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    information = []
-    let obj = {
-        id: information.length + 1,
-        name: firstName.value,
-        lastName: lastName.value,
-        study: select.value,
-        age: age.value
+
+    if(firstName.value.length > 0 && lastName.value.length > 0 && age.value.length > 0 && select.value != 'study') {
+
+        let obj = {
+            id: information.length + 1,
+            name: firstName.value,
+            lastName: lastName.value,
+            study: select.value,
+            age: age.value
+        }
+        information.push(obj)
+
+        createEl(information, list)
+    }else {
+        alert('Bo\'sh joylarni to\'liq to\'ldiring')
     }
 
-    information.push(obj)
-    createEl()
+
+    firstName.value = ''
+    lastName.value = ''
+    age.value = ''
 })
+
+buttonFilter.addEventListener('click', (e) => {
+    e.preventDefault()
+    render(information, list)
+    console.log('askl')
+    console.log(filter.value)
+})
+
+function render(arr, list) {
+    let regex = new RegExp(search.value, 'gi')
+
+    let newResult = []
+
+    let matched = arr.filter(item => item.name.match(regex))
+    console.log(matched)
+    let sorted = matched.sort((a, b) => a.age - b.age)
+
+    if(filter.value === 'filter') {
+        newResult = matched
+    }else if(filter.value === 'increase') {
+        newResult = sorted
+    }else if(filter.value === 'decrease') {
+        newResult = sorted.reverse()
+    }
+    createEl(newResult, list)
+}
